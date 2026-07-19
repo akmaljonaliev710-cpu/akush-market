3e16123f2d8fe7455b602471d2a3c60c1c7edf5a
akmaljonaliev710-cpu <akmaljonaliev710@gmail.com>
2026-07-19 10:56:52 +0300

    Create demo_categories.dart

---
+++ lib/data/demo_categories.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+final List<Category> demoCategories = [
+
+  Category(
+    name: "Mevalar",
+    icon: Icons.apple,
+  ),
+
+  Category(
+    name: "Sabzavot",
+    icon: Icons.eco,
+  ),
+
+  Category(
+    name: "Go'sht",
+    icon: Icons.restaurant,
+  ),
+
+  Category(
+    name: "Sut",
+    icon: Icons.local_drink,
+  ),
+
+  Category(
+    name: "Ichimlik",
+    icon: Icons.local_cafe,
+  ),
+
+  Category(
+    name: "Shirinlik",
+    icon: Icons.cake,
+  ),
+
+];
