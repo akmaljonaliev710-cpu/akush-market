24fd30162003c42e45ad0f9c981239f5ef3dcb84
akmaljonaliev710-cpu <akmaljonaliev710@gmail.com>
2026-07-19 10:53:43 +0300

    Create colors.dart

---
+++ lib/core/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2E7D32);
+  static const Color secondary = Color(0xFFFFD600);
+  static const Color danger = Color(0xFFE53935);
+  static const Color background = Color(0xFFF8F9FA);
+}
