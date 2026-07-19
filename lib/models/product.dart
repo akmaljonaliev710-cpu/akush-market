27c13071e0a31376bf578bddefce1880dc8103bf
akmaljonaliev710-cpu <akmaljonaliev710@gmail.com>
2026-07-19 10:50:42 +0300

    Create product.dart

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String name;
+  final String category;
+  final double price;
+  final String image;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.image,
+  });
+}
