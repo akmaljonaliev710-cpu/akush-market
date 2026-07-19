8fdb9b4bb5a082f65c8f3b311401c6a6218d7e57
akmaljonaliev710-cpu <akmaljonaliev710@gmail.com>
2026-07-19 10:51:12 +0300

    Create demo_products.dart

---
+++ lib/data/demo_products.dart
@@ -0,0 +1,37 @@
+import '../models/product.dart';
+
+final List<Product> demoProducts = [
+
+  Product(
+    id: "1",
+    name: "Olma",
+    category: "Mevalar",
+    price: 12,
+    image: "",
+  ),
+
+  Product(
+    id: "2",
+    name: "Sut",
+    category: "Sut mahsulotlari",
+    price: 18,
+    image: "",
+  ),
+
+  Product(
+    id: "3",
+    name: "Non",
+    category: "Non",
+    price: 4,
+    image: "",
+  ),
+
+  Product(
+    id: "4",
+    name: "Go'sht",
+    category: "Go'sht",
+    price: 95,
+    image: "",
+  ),
+
+];
