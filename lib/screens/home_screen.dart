2d9164e31328b0a84f58098c43e46f70fc9eb2dc
akmaljonaliev710-cpu <akmaljonaliev710@gmail.com>
2026-07-21 02:26:36 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,71 +1,48 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+import '../widgets/search_box.dart';
+import '../widgets/promo_banner.dart';
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  final List<String> _titles = [
-    "Bosh sahifa",
-    "Katalog",
-    "Savat",
-    "Profil",
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("AKUSH MARKET"),
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
-        title: Text(_titles[_selectedIndex]),
       ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            SearchBox(),
+            PromoBanner(),
 
-      body: Center(
-        child: Text(
-          _titles[_selectedIndex],
-          style: const TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
+            SizedBox(height: 20),
+
+            Text(
+              "Kategoriyalar",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "🔥 Mashhur mahsulotlar",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
-      ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        type: BottomNavigationBarType.fixed,
-
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.grid_view),
-            label: "Katalog",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Savat",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profil",
-          ),
-        ],
       ),
     );
   }
