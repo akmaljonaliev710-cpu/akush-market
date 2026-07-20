42489c0c70b0d2de235f40e86c883d424ad4b011
akmaljonaliev710-cpu <akmaljonaliev710@gmail.com>
2026-07-21 02:35:35 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../widgets/search_box.dart';
 import '../widgets/promo_banner.dart';
+import '../widgets/categories_list.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -31,6 +32,11 @@ class HomeScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(height: 20),
+
+const CategoriesList(),
+
+const SizedBox(height: 30),
 
             SizedBox(height: 20),
 
